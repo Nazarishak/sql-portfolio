b09d8b916a707a85187fa9cd55449e891702116e
Nazarishak <kiasenkonazeek@gmail.com>
2025-06-11 14:38:47 +0300

    Update create_tables.sql

--- datadase/create_tables.sql
+++ datadase/create_tables.sql
@@ -1,1 +1,18 @@
-qqq
+CREATE TABLE regions (
+    region_id INT PRIMARY KEY,
+    region_name VARCHAR(100)
+);
+
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(100),
+    region_id INT REFERENCES regions(region_id)
+);
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    salary NUMERIC(10, 2),
+    department_id INT REFERENCES departments(department_id)
+);
